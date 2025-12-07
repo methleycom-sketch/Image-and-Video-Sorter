b8e05aee0ccf4e06dd7349f9e3388d783e6f53f4
methleycom-sketch <methley.com@gmail.com>
2025-12-07 16:02:46 +0000

    Update image_sorter.py

--- image_sorter.py
+++ image_sorter.py
@@ -1,17 +1,3 @@
-def _move_to(self, folder):
-    dest_dir = self.source_dir / folder
-    dest_dir.mkdir(exist_ok=True)
-
-    dest = dest_dir / self.current_path.name
-    shutil.move(self.current_path, dest)
-
-    self.actions.append({"name": self.current_path.name, "dest": dest})
-
-    # update last folder BEFORE loading next file
-    self.last_folder_name = folder
-
-    self.index += 1
-    self.load_next_file()
 #!/usr/bin/env python3
 
 import shutil
